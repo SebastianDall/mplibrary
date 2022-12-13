e7cf2dd85ae8fe59de055c81ce3d4841f3d42bee
Sebastian Dall <semoda@bio.aau.dk>
2022-12-13 15:06:56 +0100

    First commit

---
+++ R/metadata.R
@@ -0,0 +1,66 @@
+library(tidyverse)
+
+################################
+
+#' Load metadata
+#'
+#' Loads metadata
+#'
+#'
+#'
+#' @export
+#'
+#' @example
+#' metadata <- load_metadata()
+load_metadata <- function() {
+    metadata <- read_delim("../data/metadata/selected_metadata.csv", delim = ";")
+    return(metadata)
+}
+
+################################
+#' Isolates metadata for project to be analyzed
+#' #'
+#' Functionen for isolating relevant metadata. Three projects can be used: MP or NP
+#'
+#' @param metadata A dataframe
+#' @param project_filter A project
+#'
+#' @export
+#'
+#' @example
+#' metadata_MP <- isolateProjectMetadata(metadata, "MP")
+isolateProjectMetadata <- function(metadata, project_filter = "MP") {
+    stopifnot(project_filter %in% c("MP", "NP"))
+    patientMetadataWithDonorbatches <- isolateDonorBatchesUsed(metadata, project_filter)
+
+    DonorAndPatientMetadata <- isolateDonorAndPatientMetadata(metadata, patientMetadataWithDonorbatches, project_filter) %>%
+        createXaxis()
+
+    return(DonorAndPatientMetadata)
+}
+
+isolateDonorBatchesUsed <- function(metadata, project_filter) {
+    patientMetadataWithDonorbatches <- metadata %>%
+        pivot_longer(batch_1:batch_3, names_to = "batch", values_to = "batch_number") %>%
+        filter(!is.na(batch_number), project == project_filter) %>%
+        mutate(donor_batch = paste0(donor, "_", batch_number))
+
+    return(patientMetadataWithDonorbatches)
+}
+
+isolateDonorAndPatientMetadata <- function(metadata, metadata_with_donor_batches_used, project_filter) {
+    DonorAndPatientMetadata <- metadata %>%
+        mutate(donor_batch = paste0(id, "_", fecal_donation_number)) %>%
+        filter(project == project_filter | donor_batch %in% metadata_with_donor_batches_used$donor_batch) %>%
+        mutate(group = if_else(project == "donor_batch", "FMT", group))
+
+    return(DonorAndPatientMetadata)
+}
+
+createXaxis <- function(df) {
+    dfWithXaxis <- df %>%
+        mutate(x_axis = if_else(stage == "inclusion", "Pre",
+            if_else(stage == "followup_30d", "Post", stage)
+        )) %>%
+        mutate(x_axis = if_else(project == "donor_batch", "Donor", x_axis))
+}
---
+++ tests/testthat/test-metadata_functions.R
@@ -0,0 +1,87 @@
+### Loading data
+test_that("Test correct metadata is loaded", {
+  # Dim
+  expect_equivalent(dim(load_metadata()), c(1277, 20))
+  # colnames
+  expect_setequal(
+    colnames(mplibrary::load_metadata()),
+    c("id", "sample_barcode", "LibID", "library_plate", "ext_conc", "lib_conc", "pool_ng", "stage", "project", "group", "donor", "batch_1", "batch_2", "batch_3", "fecal_donation_number", "fecal_batch_date", "pdai_score", "paired_reads", "mean_quality", "non_human_reads")
+  )
+})
+
+metadata <- load_metadata()
+
+
+# test_that("Metaphlan3 loads correctly", {
+#   expect_equivalent(dim(load_metaphlan("species")), c(505, 444))
+#   expect_equivalent(dim(load_metaphlan()), c(186, 444))
+#   expect_equivalent(dim(load_metaphlan("species")), c(505, 444))
+# })
+
+# metaphlan <- load_metaphlan("species")
+
+
+
+test_that("test if isolateDonorBatchMetadata isolates donor batches", {
+  patientMetadataWithDonorbatches <- isolateDonorBatchesUsed(metadata, project_filter = "MP")
+
+  expect_equal(all(patientMetadataWithDonorbatches$project == "MP"), TRUE)
+  expect_equal(all(str_detect(patientMetadataWithDonorbatches$donor_batch, "do\\d+")), TRUE) # donor_batch only contains do[number]
+})
+
+
+test_that("Test if isolateDonorAndPatientMetadata isolates metadata for both Patients and Donors", {
+  patientMetadataWithDonorbatches <- isolateDonorBatchesUsed(metadata, project_filter = "MP")
+
+  DonorAndPatientMetadata <- isolateDonorAndPatientMetadata(metadata, metadata_with_donor_batches_used = patientMetadataWithDonorbatches, project_filter = "MP")
+
+  expect_equivalent(unique(DonorAndPatientMetadata$project), c("donor_batch", "MP"))
+  expect_equivalent(unique(DonorAndPatientMetadata$group), c("FMT", "placebo"))
+})
+
+test_that("Test if createXaxis gives the correct Xaxis column values", {
+  # setup
+  patientMetadataWithDonorbatches <- isolateDonorBatchesUsed(metadata, project_filter = "MP")
+  DonorAndPatientMetadata <- isolateDonorAndPatientMetadata(metadata, metadata_with_donor_batches_used = patientMetadataWithDonorbatches, project_filter = "MP")
+
+  # dataframe to be tested
+  isolatedMetadatWithXaxis <- DonorAndPatientMetadata %>%
+    createXaxis()
+
+  # test
+  expect_equivalent(
+    unique(isolatedMetadatWithXaxis$x_axis),
+    c(
+      "Donor", "Pre", "Post", "treatment_5", "treatment_10", "treatment_15", "treatment_21", "followup_1m",
+      "followup_6m", "followup_12m", "drop_out", "followup_3m", "treatment_1", "treatment_2", "treatment_3", "treatment_4", "treatment_6", "treatment_7", "treatment_8",
+      "treatment_9", "treatment_11", "treatment_12", "treatment_13", "treatment_14", "followup_14d", "treatment_16", "treatment_17", "treatment_18", "treatment_19", "treatment_20", "end_of_study"
+    )
+  )
+})
+
+test_that("Test that isolateProjectMetadata works with other functions.", {
+  # setup
+  patientMetadataWithDonorbatches <- isolateDonorBatchesUsed(metadata, project_filter = "MP")
+  DonorAndPatientMetadata <- isolateDonorAndPatientMetadata(metadata, metadata_with_donor_batches_used = patientMetadataWithDonorbatches, project_filter = "MP") %>%
+    createXaxis()
+
+  # dataframe to be tested
+  isolated_metadata <- isolateProjectMetadata(metadata, project_filter = "MP")
+
+  expect_equal(all(isolated_metadata == DonorAndPatientMetadata, na.rm = T), T)
+})
+
+
+## Metaphlan functions
+
+# test_that("calculateSpeciesRichness has the correct output", {
+#   expect_equivalent(dim(calculateSpeciesRichness(metaphlan)), c(442, 2))
+#   expect_equivalent(colnames(calculateSpeciesRichness(metaphlan)), c("LibID", "richness"))
+# })
+
+
+# t_metaphlan <- transposeMetaphlan(metaphlan)
+
+# test_that("transpose Metaphlan transposes metaphlan", {
+#   expect_equivalent(sort(colnames(metaphlan)[-c(1, 2)]), rownames(t_metaphlan))
+# })
