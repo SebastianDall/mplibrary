424393b2fc10ee7b80013b79cdc514dc38197ad7
SebastianDall <sebastiandall1@hotmail.com>
2023-04-03 10:53:21 +0200

    changed metaphlan functions to accomodate new metadata

--- R/metaphlan.R
+++ R/metaphlan.R
@@ -17,6 +17,80 @@ load_metaphlan <- function(taxonomic_level = "genus") {
 
 
 
+#' Metaphlan taxonomy filter
+#'
+#' @param metaphlan 
+#' @param taxonomy 
+#' @param keep_unclassified 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filter_taxonomy <- function(metaphlan, taxonomy = "species", keep_unclassified = TRUE){
+    tax <- tibble(
+        abbr_taxonomy = c("k", "p", "c", "o", "f", "g", "s", "t"), 
+        full_taxonomy = c("kingdom", "phylum", "class", "order", "family", "genus", "species", "strain")
+    )
+    
+    tax_lvl <- filter(tax,full_taxonomy == taxonomy)$abbr_taxonomy
+    next_tax_lvl <- filter(tax, abbr_taxonomy == tax$abbr_taxonomy[grep(tax_lvl, tax$abbr_taxonomy) + 1])$abbr_taxonomy
+    
+    
+    if(keep_unclassified){
+        df <- metaphlan %>% 
+            filter(
+                clade_name == "UNCLASSIFIED" | str_detect(clade_name, paste0(tax_lvl,"_{2}")), 
+                str_detect(clade_name, paste0(next_tax_lvl,"_{2}")) == FALSE
+            )
+    } else {
+        df <- metaphlan %>% 
+            filter(
+                str_detect(clade_name, paste0(tax_lvl,"_{2}")), 
+                str_detect(clade_name, paste0(next_tax_lvl,"_{2}")) == FALSE
+            )
+    }
+    
+    if(taxonomy == "strain"){
+        df <- metaphlan %>% 
+            filter(
+                clade_name == "UNCLASSIFIED" | str_detect(clade_name, paste0(tax_lvl,"_{2}"))
+            )
+    }
+    
+    
+    return(
+        df %>% 
+            mutate(
+                clade_name = str_remove(clade_name, paste0(".*",tax_lvl,"__"))
+            )
+    )
+    
+    
+}
+
+
+#' Rename columns in metaphlan
+#'
+#' @param metaphlan 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+remove_NonHuman_from_colnames <- function(metaphlan){
+    colnames_old <- colnames(metaphlan)
+    colnames_new <- str_remove(colnames_old, "\\_Non.*")
+    
+    
+    return(
+        metaphlan %>% 
+            rename_at(colnames_old, ~ colnames_new)
+    )
+}
+
+
+
 #' Calculate the species richness
 #'
 #' Calculate the species richness defined as number of species present in a sample. Filter_species will first filter all species with less than x relative abundance in at least one sample
@@ -30,9 +104,10 @@ load_metaphlan <- function(taxonomic_level = "genus") {
 #' species_richness <- calulateSpeciesRichness(metaphlan, filter_species = 0.1)
 calculateSpeciesRichness <- function(metaphlan, filter_species = 0) {
     species_count_for_each_sample <- metaphlan %>%
-        select(-NCBI_tax_id) %>%
-        pivot_longer(-clade_name, names_to = "LibID") %>%
-        group_by(LibID) %>%
+        filter(clade_name != "UNCLASSIFIED") %>% 
+        #select(-NCBI_tax_id) %>%
+        pivot_longer(-clade_name, names_to = "sample_barcode") %>%
+        group_by(sample_barcode) %>%
         summarise(richness = sum(value > filter_species))
 
     return(species_count_for_each_sample)
@@ -50,11 +125,11 @@ calculateSpeciesRichness <- function(metaphlan, filter_species = 0) {
 #' t_metaphlan <- transposeMetaphlan(metaphlan)
 transposeMetaphlan <- function(metaphlan) {
     t_metaphlan <- metaphlan %>%
-        select(-NCBI_tax_id) %>%
-        pivot_longer(-clade_name, names_to = "LibID") %>%
+        #select(-NCBI_tax_id) %>%
+        pivot_longer(-clade_name, names_to = "sample_barcode") %>%
         pivot_wider(names_from = clade_name, values_from = value) %>%
-        arrange(LibID) %>%
-        column_to_rownames(var = "LibID")
+        arrange(sample_barcode) %>%
+        column_to_rownames(var = "sample_barcode")
 
 
     return(t_metaphlan)
--- tests/testthat/test-metaphlan_functions.R
+++ tests/testthat/test-metaphlan_functions.R
@@ -1,4 +1,4 @@
-setwd("../..")
+setwd("../../src/")
 
 
 test_that("Metaphlan3 loads correctly", {
@@ -7,19 +7,44 @@ test_that("Metaphlan3 loads correctly", {
     expect_equal(dim(load_metaphlan("species")), c(505, 444))
 })
 
-metaphlan <- load_metaphlan("species")
+# metaphlan <- load_metaphlan("species")
+metaphlan <- readr::read_delim(
+    "../data/metaphlan3/MetaPhlAn_4.0.3_Combined_NonHuman_Subsampled_2500000_profile.txt", 
+    delim = "\t", 
+    show_col_types = FALSE, 
+    skip = 1
+)
 
+test_that("Filter species gives expected dim", {
+    expect_equal(dim(filter_taxonomy(metaphlan, "species", TRUE)), c(1306, 646))
+    expect_equal(dim(filter_taxonomy(metaphlan, "species", FALSE)), c(1305, 646))
+})
+
+test_that("Filter species gives expected sum", {
+    expect_equal(sum(filter_taxonomy(metaphlan, "species", TRUE)[,2]), 100, tolerance = 0.001)
+    expect_equal(sum(filter_taxonomy(metaphlan, "species", FALSE)[,2]) == 100, FALSE)
+})
+
+test_that("remove_NonHuman_from_colnames - removes NonHuman", {
+    expect_equal(all(is.na(str_match(colnames(remove_NonHuman_from_colnames(metaphlan)), "NonHuman"))), TRUE)
+})
+
+
+metaphlan <- metaphlan %>% 
+    filter_taxonomy("species", TRUE) %>% 
+    remove_NonHuman_from_colnames()
 
 # Metaphlan functions
 
 test_that("calculateSpeciesRichness has the correct output", {
-    expect_equal(dim(calculateSpeciesRichness(metaphlan)), c(442, 2))
-    expect_equal(colnames(calculateSpeciesRichness(metaphlan)), c("LibID", "richness"))
+    s <- calculateSpeciesRichness(metaphlan)
+    expect_equal(s$richness[1], sum(metaphlan[s$sample_barcode[1]]>0) -1)
+    
 })
 
 
 t_metaphlan <- transposeMetaphlan(metaphlan)
 
 test_that("transpose Metaphlan transposes metaphlan", {
-    expect_equal(sort(colnames(metaphlan)[-c(1, 2)]), rownames(t_metaphlan))
+    expect_equal(sort(colnames(metaphlan)[-1]), rownames(t_metaphlan))
 })
